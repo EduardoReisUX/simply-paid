2d8cc6e68d1330a081c6c67c3f932c2570b58a2d
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-01-18 23:01:08 -0400

    feat: add user entity implementation

---
+++ src/modules/users/core/Users.ts
@@ -0,0 +1,77 @@
+export interface IUser {
+  name: string;
+  lastname: string;
+  document: string;
+  email: string;
+  password: string;
+  role: "common" | "shopkeeper";
+}
+
+export class User {
+  id;
+  name;
+  lastname;
+  document;
+  email;
+  password;
+  role;
+
+  constructor(user: IUser) {
+    this.id = crypto.randomUUID();
+    this.name = user.name;
+    this.lastname = user.lastname;
+    this.document = user.document;
+    this.email = user.email;
+    this.password = user.password;
+    this.role = user.role;
+  }
+
+  public static create(user: IUser) {
+    const document = this.validateDocument(user.document);
+
+    if (!document.isValid) {
+      return document.errors;
+    }
+
+    const role = this.validateRole(user.role);
+
+    if (!role.isValid) {
+      return role.errors;
+    }
+
+    return new User(user);
+  }
+
+  private static validateDocument(document: string) {
+    const errors = [];
+
+    if (isNaN(Number(document))) {
+      errors.push({
+        name: "InvalidFormatError",
+        message: `The user document [${document}] must have only numbers!`,
+      });
+    }
+
+    if (document.length !== 11) {
+      errors.push({
+        name: "InvalidLengthError",
+        message: `The user document [${document}] must have 11 digits!`,
+      });
+    }
+
+    return { errors, isValid: !errors.length };
+  }
+
+  private static validateRole(role: string) {
+    const errors = [];
+
+    if (role !== "common" && role !== "shopkeeper") {
+      errors.push({
+        name: "InvalidFormatError",
+        message: `The user role [${role}] should have only numbers!`,
+      });
+    }
+
+    return { errors, isValid: !errors.length };
+  }
+}
