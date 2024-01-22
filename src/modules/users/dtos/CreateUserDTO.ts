a2a8694ce3e77b51c7168924fda42d53079e7a90
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-01-22 15:40:12 -0400

    feat: add a check verifying if user has valid data

--- src/modules/users/dtos/CreateUserDTO.ts
+++ src/modules/users/dtos/CreateUserDTO.ts
@@ -5,4 +5,5 @@ export interface CreateUserDTO {
   email: string;
   password: string;
   role: "common" | "shopkeeper";
+  funds?: number;
 }
--- src/modules/users/services/UsersService/UsersService.ts
+++ src/modules/users/services/UsersService/UsersService.ts
@@ -1,3 +1,4 @@
+import { User } from "../../domain/Users";
 import { CreateUserDTO } from "../../dtos/CreateUserDTO";
 import { IUsersRepository } from "../../repository/IUsersRepository";
 
@@ -8,23 +9,37 @@ export class UsersService {
     this.usersRepository = usersRepositoy;
   }
 
-  async create(user: CreateUserDTO) {
+  async create(data: CreateUserDTO) {
     const documentAlreadyExists = await this.usersRepository.getUserByDocument(
-      user.document
+      data.document
     );
 
     if (documentAlreadyExists) {
-      return { error: `user document [${user.document}] already exists!` };
+      return { error: `user document [${data.document}] already exists!` };
     }
 
     const emailAlreadyExists = await this.usersRepository.getUserByEmail(
-      user.email
+      data.email
     );
 
     if (emailAlreadyExists) {
-      return { error: `user email [${user.email}] already exists!` };
+      return { error: `user email [${data.email}] already exists!` };
+    }
+
+    const user = User.create(data);
+
+    if (Array.isArray(user)) {
+      return user.map((error) => error);
     }
 
     this.usersRepository.save(user);
+  }
+
+  async findById(id: string) {
+    return await this.usersRepository.getUserById(id);
+  }
+
+  async findByDocument(document: string) {
+    return await this.usersRepository.getUserByDocument(document);
   }
 }
