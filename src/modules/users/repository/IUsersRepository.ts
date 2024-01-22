3ea12fb5668710839b7d5f3bb5ae6fa30db1ed9e
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-01-22 16:11:21 -0400

    fix: change user repository save method param type

--- src/modules/users/repository/IUsersRepository.ts
+++ src/modules/users/repository/IUsersRepository.ts
@@ -1,10 +1,9 @@
 import { User } from "../domain/Users";
-import { CreateUserDTO } from "../dtos/CreateUserDTO";
 
 export interface IUsersRepository {
   getUserById(id: string): Promise<User | null>;
   getUserByEmail(email: string): Promise<User | null>;
   getUserByDocument(document: string): Promise<User | null>;
   listAllUsers(): Promise<User[]>;
-  save(user: CreateUserDTO): Promise<void>;
+  save(user: User): Promise<void>;
 }
