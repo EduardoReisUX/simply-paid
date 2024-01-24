aa74c5b45a904ff3220faf075cf758f207205559
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-01-24 13:23:29 -0400

    refactor: implement result pattern to transaction service

--- src/modules/transactions/services/TransactionsService.ts
+++ src/modules/transactions/services/TransactionsService.ts
@@ -1,3 +1,4 @@
+import { Result } from "../../../shared/Result";
 import { UsersService } from "../../users/services/UsersService/UsersService";
 import { Transaction } from "../domain/Transactions";
 import { CreateTransactionDTO } from "../dtos/CreateTransaction";
@@ -20,38 +21,50 @@ export class TransactionsService {
     receiver_document,
     amount,
   }: CreateTransactionDTO) {
-    const sender = await this.usersService.findByDocument(sender_document);
+    const senderOrError = await this.usersService.findByDocument(
+      sender_document
+    );
 
-    if (!sender) {
-      return { error: "Sender not found!" };
+    if (senderOrError.isFailure) {
+      return Result.fail(["Sender not found!"]);
     }
 
-    const receiver = await this.usersService.findByDocument(receiver_document);
+    const sender = senderOrError.getValue();
 
-    if (!receiver) {
-      return { error: "Receiver not found!" };
+    const receiverOrError = await this.usersService.findByDocument(
+      receiver_document
+    );
+
+    if (receiverOrError.isFailure) {
+      return Result.fail(["Receiver not found!"]);
     }
+
+    const receiver = receiverOrError.getValue();
 
     if (sender.role === "shopkeeper") {
-      return { error: `Shopkeepers [${sender.name}] can't transfer funds!` };
+      return Result.fail([
+        `Shopkeepers [${sender.name}] can't transfer funds!`,
+      ]);
     }
 
     if (sender.funds < Number(amount)) {
-      return {
-        error: `Sender [${sender.name}] does not have sufficient funds!`,
-      };
+      return Result.fail([
+        `Sender [${sender.name}] does not have sufficient funds!`,
+      ]);
     }
 
-    const transaction = Transaction.create({
+    const transactionOrError = Transaction.create({
       receiver_id: receiver.id,
       sender_id: sender.id,
       amount,
     });
 
-    if (Array.isArray(transaction)) {
-      return transaction.map((error) => error);
+    if (transactionOrError.isFailure) {
+      return Result.fail(transactionOrError.errors);
     }
 
-    this.transactionsRepository.save(transaction);
+    const transaction = transactionOrError.getValue();
+
+    return Result.ok(await this.transactionsRepository.save(transaction));
   }
 }
