763d480b58ac5cdc9e194285853e7a67633f1147
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-02-15 22:46:12 -0400

    test: add e2e tests for given invalid data for creating user

--- src/shared/infra/http/node/app.e2e.spec.ts
+++ src/shared/infra/http/node/app.e2e.spec.ts
@@ -2,7 +2,7 @@ import { expect, describe, it, beforeAll, afterAll } from "vitest";
 
 const BASE_URL = "http://localhost:3000";
 
-describe("User route", () => {
+describe("/users", () => {
   let _server: import("node:http").Server;
 
   beforeAll(async () => {
@@ -12,7 +12,7 @@ describe("User route", () => {
 
   afterAll(() => _server.close());
 
-  describe("/post", () => {
+  describe("POST", () => {
     it("should create a user given valid data", async () => {
       const data = {
         name: "dudu",
@@ -28,19 +28,57 @@ describe("User route", () => {
         body: JSON.stringify(data),
       });
 
-      const result = await request.json();
-      const user = result.getValue();
-
       expect(request.status).toBe(201);
+    });
 
-      expect(user).toHaveProperty("id");
-      expect(user).toHaveProperty("name");
-      expect(user).toHaveProperty("lastname");
-      expect(user).toHaveProperty("email");
-      expect(user).toHaveProperty("password");
-      expect(user).toHaveProperty("document");
-      expect(user).toHaveProperty("role");
-      expect(user).toHaveProperty("funds");
+    it("should not create a user given any invalid data", async () => {
+      const data = {
+        name: "dudu",
+        lastname: "dos reis",
+        email: "unique2@email.com",
+        password: "dudu",
+        document: "12345a",
+        role: "asdf",
+      };
+
+      const request = await fetch(`${BASE_URL}/users`, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+
+      const response = await request.json();
+
+      expect(request.status).toBe(400);
+      expect(response).toStrictEqual({
+        errors: [
+          "InvalidFormatError: The user document [12345a] must have only numbers!",
+          "InvalidLengthError: The user document [12345a] must have 11 digits!",
+          "InvalidFormatError: The user role [asdf] should be 'common' or 'shopkeeper' only!",
+        ],
+      });
+    });
+
+    it("should not create a user given user already exists", async () => {
+      const data = {
+        name: "dudu",
+        lastname: "dos reis",
+        email: "unique@email.com",
+        password: "dudu",
+        document: "12345678911",
+        role: "common",
+      };
+
+      const request = await fetch(`${BASE_URL}/users`, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+
+      const response = await request.json();
+
+      expect(request.status).toBe(400);
+      expect(response).toStrictEqual({
+        errors: ["user document [12345678911] already exists!"],
+      });
     });
   });
 });
