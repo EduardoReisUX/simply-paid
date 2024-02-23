29e597287a320bc34989fcdc415d311305c6dd87
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-02-22 22:07:38 -0400

    feat: add POST and GET methods for /users route

--- src/shared/infra/http/express/routes/user.routes.ts
+++ src/shared/infra/http/express/routes/user.routes.ts
@@ -26,3 +26,21 @@ usersRoute.post("/", async (request: Request, response: Response) => {
 
   return response.status(201).send();
 });
+
+usersRoute.get("/:document", async (request: Request, response: Response) => {
+  const { document } = request.params;
+
+  if (typeof document !== "string") {
+    return response.status(400).send({
+      errors: `InvalidFormatError: Document ${document} is not a string!`,
+    });
+  }
+
+  const successOrFailure = await usersService.findByDocument(document);
+
+  if (!successOrFailure || successOrFailure.isFailure) {
+    return response.status(400).send({ errors: successOrFailure.errors });
+  }
+
+  return response.status(200).send({ user: successOrFailure.getValue() });
+});
