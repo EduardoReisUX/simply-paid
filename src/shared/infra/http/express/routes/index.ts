f7bd3783e5292be851d42ba6129bb272f6271ac4
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-02-24 16:59:55 -0400

    feat: add /transactions routes to Router list

--- src/shared/infra/http/express/routes/index.ts
+++ src/shared/infra/http/express/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { usersRoute } from "./user.routes";
+import { usersRoute } from "./users/users.routes";
+import { transactionsRoute } from "./transactions/transactions.routes";
 
 export const router = Router();
 
 router.use("/users", usersRoute);
+router.use("/transactions", transactionsRoute);
